12a510ace85d4b2a96565aa36be79428d9f1e72a
Forrest Fleming <ffleming@gmail.com>
2016-10-24 21:07:08 -0700

    daemonization & argument parsing

--- README.md
+++ README.md
@@ -1,27 +1,21 @@
 Shrimpy
 ===
 
-Shrimpy is an http server.  It is called 'Shrimpy' because the Rust community
-has an affinity for crustaceans and one of my son's favorite toys is a small
-stuffed shrimp.
+Shrimpy is an http server.  It is called 'Shrimpy' because the Rust community has an affinity for crustaceans and one of my son's favorite toys is a small stuffed shrimp.
 
-Also, shrimpy is very small and, well, shrimpy.
+Also, Shrimpy is very small and shrimpy.
 
-Shrimpy exists solely for me to play around with Rust.  Its aim is to get a
-functioning server that is capable of serving statically rendered web pages.
+Shrimpy exists solely for me to play around with Rust.  Because the point is to get familiar with the language, Shrimpy does not use any crates.
 
-Shrimpy will probably never support verbs beyond GET and, if I'm feeling
-especially sassy, POST.
-
-Shrimpy does not use any crates, with the possible exception of libc so I can
-use `fork()`.  If I'm being realistic, though, Shrimpy probably won't make it
-that far.
+Shrimpy will probably never support verbs beyond GET and, if I'm feeling especially sassy, POST.
 
 TODO
 ---
-1. Maybe don't allow path traversal (fun fact: I couldn't trivially trigger
-   this, but it has to be there.  It just has to.)
-2. Struct for server
+1. Maybe don't allow path traversal (fun fact: I couldn't trivially trigger this, but it has to be there.  It just has to.)
+2. ~~Module & struct for server~~
 3. Configurable root directory, serve files from it
 4. Logging that isn't `println!()`
-5. Privilege dropping in child
+5. ~~Daemonization~~
+6. Privilege dropping in child
+7. ~~Argument parsing / help text~~
+8. Module & struct for argument parser
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 pub mod http;
 pub mod os_ffi;
 use std::process::exit;
+use std::env::args;
+use std::collections::HashMap;
 use http::server::HttpServer as HttpServer;
 
 fn main() {
@@ -9,9 +11,29 @@ fn main() {
         host: String::from("127.0.0.1"),
         root: String::from("src/")
     };
+    let arguments = argument_hash();
+    let false_string = String::from("false");
+    let help = arguments.get("help").unwrap_or(&false_string);
+    let daemonize = arguments.get("daemonize").unwrap_or(&false_string);
+    if help == "true" {
+        print_help();
+        exit(0);
+    }
+
+    run_server(server, daemonize == "true");
+}
+
+fn run_server(server: HttpServer, daemonize: bool) {
+    if daemonize {
+        daemonize_server(server);
+    } else {
+        server.run();
+    }
+}
+
+fn daemonize_server(server: HttpServer) {
     unsafe {
         let pid = os_ffi::fork();
-        println!("woof woof {}", pid);
         match pid {
             -1 => panic!("Fork failed!"),
             0  => server.run(),
@@ -21,6 +43,54 @@ fn main() {
                     exit(0);
                 }
         }
-        server.run();
     }
+}
+
+fn argument_hash() -> HashMap<String, String> {
+    let mut arg_hash: HashMap<String, String> = HashMap::new();
+    let args_vec: Vec<String> = args().collect();
+    let mut key: &str; //= "";
+    let mut val: &str; // = "";
+
+    for i in 1..args_vec.len() {
+        let ref word = args_vec[i];
+        if word.starts_with("--") {
+            if word == "--daemonize" {
+                key = &"daemonize";
+                val = &"true";
+            } else if word == "--help" {
+                key = &"help";
+                val = &"true";
+            } else {
+                let keyval_pair: Vec<&str> = word.split("=").collect();
+                key = keyval_pair[0];
+                val = keyval_pair[1];
+            }
+        } else if word.starts_with("-") {
+            if word == "-d" {
+                key = &"daemonize";
+                val = &"true";
+            } else if word == "-h" {
+                key = &"help";
+                val = &"true";
+            } else {
+                key = &args_vec[i];
+                val = &args_vec[i + 1];
+            }
+        } else {
+            continue;
+        }
+        arg_hash.insert(String::from(key), String::from(val));
+    }
+    return arg_hash;
+}
+
+fn print_help() {
+    let prog_full_path: String = args().next().unwrap();
+    let prog_name = prog_full_path.split("/").last().unwrap();
+    println!("Usage {} [OPTION]", prog_name);
+    println!("Run shrimpy http server");
+    println!("");
+    println!("  -d, --daemonize\t\t Run server in background");
+    println!("  -h, --help\t\t\t This help");
 }
