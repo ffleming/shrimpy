6beffd67f46a1c01c2aff7fef1fc96cb1c27dd0d
Forrest Fleming <ffleming@gmail.com>
2016-11-29 20:23:16 -0800

    Add bind host and port options

--- src/main.rs
+++ src/main.rs
@@ -12,30 +12,38 @@ fn main() {
         key: String::from("help"),
         long_argument: String::from("help"),
         short_argument: String::from("h"),
-        // default: String::from("false"),
-        // name: String::from("Help"),
         takes_value: false,
-        // required: false
     }).with_arg(Argument {
         key: String::from("daemonize"),
         long_argument: String::from("daemonize"),
         short_argument: String::from("d"),
-        // default: String::from("false"),
-        // name: String::from("Daemonize"),
         takes_value: false,
-        // required: false
     }).with_arg(Argument {
         key: String::from("root"),
         long_argument: String::from("root"),
         short_argument: String::from("r"),
         takes_value: true,
+    }).with_arg(Argument {
+        key: String::from("port"),
+        long_argument: String::from("port"),
+        short_argument: String::from("p"),
+        takes_value: true,
+    }).with_arg(Argument {
+        key: String::from("host"),
+        long_argument: String::from("bindhost"),
+        short_argument: String::from("b"),
+        takes_value: true,
     }).as_hash();
 
     let false_string = String::from("false");
     let default_root = String::from("/shrimpy/");
+    let default_port = String::from("8080");
+    let default_host = String::from("localhost");
     let help = arguments.get("help").unwrap_or(&false_string);
     let daemonize = arguments.get("daemonize").unwrap_or(&false_string);
     let root = arguments.get("root").unwrap_or(&default_root);
+    let port = arguments.get("port").unwrap_or(&default_port);
+    let host = arguments.get("host").unwrap_or(&default_host);
 
     if help == "true" {
         print_help();
@@ -43,8 +51,8 @@ fn main() {
     }
 
     let server = HttpServer {
-        port: String::from("8080"),
-        host: String::from("127.0.0.1"),
+        port: (*port).clone(),
+        host: (*host).clone(),
         root: (*root).clone()
     };
 
@@ -82,5 +90,7 @@ fn print_help() {
     println!("");
     println!("  -d, --daemonize\t\t Run server in background");
     println!("  -r, --root\t\t\t Root path from which to serve files");
+    println!("  -b, --bindhost\t\t Host to bind");
+    println!("  -p, --port\t\t\t Port to listen on");
     println!("  -h, --help\t\t\t This help");
 }
